80e280e7071ee0b3c9fadf3cbc469f5788db3a71
AndrewLa <andrew.lzrv@gmail.com>
2024-04-04 14:43:45 +0700

    Added tests to scoped_ptr

--- scoped_ptr_nstt5/main.cpp
+++ scoped_ptr_nstt5/main.cpp
@@ -1,18 +1,47 @@
 #include "library/scoped_ptr.hpp"
 #include <iostream>
 
-class A {
-    public:
-    int pi = 3;
-    
-    A() = default;
+int counter;
 
-    ~A() {
-        std::cout << "Hello World!" << '\n';
+class mock {
+    public:
+    mock() = default;
+    mock(const mock&) = default;
+    mock(mock&&) = default;
+
+    ~mock() {
+        counter++;
     }
+
+    mock& operator=(const mock&) = default;
+    mock& operator=(mock&&) = default;
 };
 
 int main(int, char**) {
-    ScopedPtrCopy<A> a{new A()};
-    (*a).pi = 34;
+    counter = 0;
+
+    {
+        ScopedPtrCopy<mock> ptr1{new mock{}};
+        ScopedPtrCopy<mock> ptr2 = ptr1;
+        ScopedPtrCopy<mock> ptr3 = std::move(ptr1);
+        ScopedPtrCopy<mock> ptr4 = ptr2;
+
+        std::cout << counter << " -> ";
+    }
+
+    std::cout << counter << std::endl;
+
+    counter = 0;
+
+    {
+        ScopedPtrMove<mock> ptr1{new mock{}};
+        ScopedPtrMove<mock> ptr2{std::move(ptr1)};
+        ScopedPtrMove<mock> ptr3{std::move(ptr2)};
+
+        std::cout << counter << " -> ";
+    }
+
+    std::cout << counter << std::endl;
+
+    return 0;
 }
